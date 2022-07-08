6aa269c7f3d882d29f4ee9fe11e8c16166209dc9
Herve Abdi <herve@utdallas.edu>
2022-07-08 16:40:06 -0500

    petites choses

--- R/R4SPISE2022.R
+++ R/R4SPISE2022.R
@@ -6,6 +6,10 @@
 #' and Data for the SPISE2022 Advanced Workshop
 #' (Hanoi Vietnam).
 #'
+#' @description A set of Programs, Vignettes,
+#' and Data for the SPISE2022 Advanced Workshop
+#' (Hanoi Vietnam): 07/27/2022 to 07/29/2022.
+#'
 #' @docType package
 #' @name R4SPISE2022-package
 #' @aliases R4SPISE2022
