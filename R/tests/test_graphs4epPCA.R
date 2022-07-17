abb6a07df646672c319fa636f88195e34525891f
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2022-07-17 22:16:14 +0200

    UPDATE vignette for PCA on beers

--- R/tests/test_graphs4epPCA.R
+++ R/tests/test_graphs4epPCA.R
@@ -8,6 +8,11 @@ I <- 57
 J <- 11
 X <- matrix(rnorm(I * J), I, J)
 
-res.epPCA <- epPCA(DATA = X, graph = FALSE)
-res.f1 <- graph4epPCA(data = X)
+res.epPCA <- epPCA(DATA = X, center = TRUE, scale = "ss1", graph = FALSE)
+res.f1 <- graph4epPCA(data = X, center = TRUE, scale = "ss1")
 res.f2 <- OTAplot(res = res.epPCA, data = X)
+
+res.f1$results.graphs$scree
+res.f2$results.graphs$scree
+res.f1$results.graphs$cosineCircleArrowJ12
+res.f2$results.graphs$cosineCircleArrowJ12
