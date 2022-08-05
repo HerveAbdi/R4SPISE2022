12b958b15cd6d86b22c7ddc0e131ba25aa4af997
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2022-08-05 17:10:40 +0200

    FIX OTA plot inference

--- R/tests/test_graphs4epPCA.R
+++ R/tests/test_graphs4epPCA.R
@@ -13,24 +13,13 @@ X <- matrix(rnorm(I * J), I, J)
 
 res.epPCA <- epPCA(DATA = X, center = TRUE, scale = "ss1", graph = FALSE)
 # res.f1 <- graph4epPCA(data = X, center = TRUE, scale = "ss1")
-res.plot <- OTAplot(res = res.epPCA, data = X)
-res.plot.inference <- OTAplotInference(res = res.epPCA, data = X)
+res.plot <- OTAplot(resPCA = res.epPCA, data = X)
+res.plot.inference <- OTAplotInference(resPCA = res.epPCA, data = X)
 
 res.plot$results.graphs$scree
+res.plot$results.graphs$factorScoresI12
 res.plot$results.graphs$cosineCircleArrowJ12
 
-##### Inference and bootstrap
-bootstapped.Eigenvalues <- Boot4Eigs(
-    X,center = TRUE, scale = "ss1", niter = 100,
-    CI.perc = c(0.025, 0.975))
-
-## Scree with confidence intervals
-PlotScreeWithCI(
-    ev = bootstapped.Eigenvalues$BootstrappedEstEigs,
-    ci.ev = bootstapped.Eigenvalues)
-
-
-
-
-
-
+res.plot.inference$results.graphs$scree
+res.plot.inference$results.graphs$BR1
+res.plot.inference$results.graphs$BR2
